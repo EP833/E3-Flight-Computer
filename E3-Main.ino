59b39acd5630312a14e1b5d828c48e02a451b0b6
Erick Padilla <erickpadilla1945@gmail.com>
2023-10-31 01:38:10 -0700

    Update E3-Main.ino
    Data Logging now working
    Pressure Sensor now working

--- E3-Main.ino
+++ E3-Main.ino
@@ -1,9 +1,17 @@
-#include <SimpleKalmanFilter.h>
-#include <Arduino_LSM9DS1.h>
-#include <Arduino_LPS22HB.h>    // Baro
+#include <Arduino_LPS22HB.h>
+#include <SPI.h>                // SPI Communication 
+#include <SD.h>                 // SD Card
+#include <SimpleKalmanFilter.h> // Kalman Filter
+#include <Arduino_LSM9DS1.h>    // IMU
 
 
 boolean viewInSerialPlotter=true;      //true optimises for serial plotter, false for serial monitor
+
+// set up variables using the SD utility library functions:
+Sd2Card card;
+SdVolume volume;
+SdFile root;
+File myFile;
 
 // Setup for Kalman Filters
 SimpleKalmanFilter XgyroFilter(2, 2, 0.05);
@@ -12,7 +20,7 @@ SimpleKalmanFilter ZgyroFilter(.6, .6, 0.3);
 SimpleKalmanFilter XacelFilter(.1, .1, 0.17);
 SimpleKalmanFilter YacelFilter(.1, .1, 0.17);
 SimpleKalmanFilter ZacelFilter(.1, .1, 0.17);
-SimpleKalmanFilter Pres(3,3,0.25);
+SimpleKalmanFilter PresFilter(.1,.1,0.25);
 
 
 // Setup variables
@@ -20,11 +28,40 @@ bool launched = false;
 unsigned long CurTime;
 unsigned long TimeCheck;
 float ZgStill, ZgUpper, ZgLowwer;
+const int chipSelect = 10;
+float Xacel, Yacel, Zacel, Xgyro, Ygyro, Zgyro, XgyroVal, YgyroVal, ZgyroVal, XacelVal, YacelVal, ZacelVal;
+
 
 void setup() 
 {  
   Serial.begin(9600);
   while (!Serial);
+
+  //******************************************  SD  *******************************************************
+
+
+  Serial.print("\nInitializing SD card...");
+
+
+  if (!SD.begin(10)) 
+  {
+    Serial.println("initialization failed!");
+    while (1);
+  }
+  Serial.println("initialization done.");
+
+  myFile = SD.open("data2.csv", FILE_WRITE);
+
+  // if the file opened okay, write to it:
+  if (myFile) {
+    Serial.println("opened success");
+  } else {
+    // if the file didn't open, print an error:
+    Serial.println("error opening data2.txt");
+    while (1);
+  }
+
+  //******************************************  IMU  *******************************************************
 
   if (!IMU.begin())
   { 
@@ -32,12 +69,7 @@ void setup()
     while (1);
   }
 
-  if (!BARO.begin()) 
-  {
-    Serial.println("Failed to initialize pressure sensor!");
-    while (1);
-  }
-
+  
   IMU.setAccelFS(1);
   IMU.setAccelODR(5);
   IMU.setAccelOffset(-0.010507, -0.015193, -0.010510);
@@ -48,10 +80,19 @@ void setup()
   *******  ODR Output Data Rate   range 0:off | 1:10Hz | 2:50Hz | 3:119Hz | 4:238Hz | 5:476Hz, (default=3)(not working 6:952Hz) ******
   ************************************************************************************************************************************/
 
-  IMU.setGyroFS(2);
-  IMU.setGyroODR(5);
-  IMU.setGyroOffset (2.105325, 0.770813, -1.001007);
-  IMU.setGyroSlope (1.193348, 1.159747, 1.147723);
+  //******************************************  BARO  *******************************************************
+
+
+  if (!BARO.begin()) {
+    Serial.println("Failed to initialize pressure sensor!");
+    while (1);
+  }
+
+  // Gyroscope code
+   IMU.setGyroFS(1);
+   IMU.setGyroODR(5);
+   IMU.setGyroOffset (0.884926, 0.944702, -1.495347);
+   IMU.setGyroSlope (1.184391, 1.239955, 1.191125);
 
 /*****************************************************************************************************************************     
 *********  FS  Full Scale       setting 0: ±245°/s | 1: ±500°/s | 2: ±1000°/s | 3: ±2000°/s       ****************************
@@ -76,22 +117,27 @@ void setup()
   Serial.print(IMU.getGyroODR());        //alias  IMU.gyroscopeSampleRate());
   Serial.println(" Hz");
 
-   Serial.println(" X \t Y \t Z ");
+  Serial.println(" X \t Y \t Z ");
+
+  myFile.println("Pressure, Kalman Pressure, Altitude, X Accel, Y Accel, Z Accel, X Gyro, Y Gyro, Z Gyro,");
 }
 
 void loop() 
 {
-  float Xacel, Yacel, Zacel, Xgyro, Ygyro, Zgyro, XgyroVal, YgyroVal, ZgyroVal, XacelVal, YacelVal, ZacelVal;
-  /*
-  float pressure = BARO.readPressure()*10;
-  float kpress = Pres.updateEstimate(pressure);
+  float pressure = BARO.readPressure();  
   Serial.print(pressure);
   Serial.print('\t');
+
+  float kpress;
+  kpress = PresFilter.updateEstimate(pressure);
   Serial.print(kpress);
   Serial.print('\t');
-  float alt = 145366.45*(1-pow((kpress/1013.25),0.190284));
+  
+
+  float alt = 145366.45*(1-pow(((kpress*10)/1013.25),0.190284));
   Serial.println(alt);
-  */
+  Serial.print('\t');
+  
 
   if (IMU.accelAvailable())                   // alias IMU.accelerationAvailable in library version 1.01
   {  
@@ -99,25 +145,8 @@ void loop()
     XacelVal = XacelFilter.updateEstimate(Xacel);
     YacelVal = YacelFilter.updateEstimate(Yacel);
     ZacelVal = ZacelFilter.updateEstimate(Zacel);
-    /*
-    Serial.print(-3);
-    Serial.print('\t');
-    Serial.print(3);
-    Serial.print('\t');
-    Serial.print(Xacel);
-    Serial.print('\t');
-    Serial.print(XacelVal);
-    Serial.print('\t');
-    Serial.print(Yacel);
-    Serial.print('\t');
-    Serial.print(YacelVal);
-    Serial.print('\t');
-    Serial.print(Zacel);
-    Serial.print('\t');
-    Serial.print(ZacelVal);
-    Serial.println('\t');
-    */  
   }
+  Serial.print('\t');
   
   if (IMU.gyroAvailable())    // alias IMU.gyroscopeAvailable
   {  
@@ -125,29 +154,9 @@ void loop()
     XgyroVal = XgyroFilter.updateEstimate(Xgyro);
     YgyroVal = YgyroFilter.updateEstimate(Ygyro);
     ZgyroVal = ZgyroFilter.updateEstimate(Zgyro);
-    
-    /*
-    Serial.print(-3);
-    Serial.print('\t');
-    Serial.print(3);
-    Serial.print('\t');
-    Serial.print(Xgyro);
-    Serial.print('\t');
-    Serial.print(XgyroVal);
-    Serial.print('\t');
-    Serial.print(Ygyro);
-    Serial.print('\t');
-    Serial.print(YgyroVal);
-    Serial.print('\t');
-    Serial.print(Zgyro);
-    Serial.print('\t');
-    Serial.println(ZgyroVal);
-    */
 
   }
   
-
-
   if (ZacelVal > 1.1 && launched == false)
   {
     launched = true;
@@ -157,41 +166,46 @@ void loop()
     ZgUpper = (ZacelVal + 1) * 1.01;
     ZgLowwer = (ZacelVal + 1)  * 0.99;
   }
+  Serial.print('\t');
 
   if (launched)
   {
+    String stringPres = String(pressure);
+    String stringKPres = String(kpress);
+    String stringAlt = String(alt);
+    String stringXAcel = String(XacelVal);
+    String stringYAcel = String(YacelVal);
+    String stringZAcel = String(ZacelVal);
+    String stringXGyro = String(XgyroVal);
+    String stringYGyro = String(YgyroVal);
+    String stringZGyro = String(ZgyroVal);
+    String dataString = String(stringPres + "," + stringKPres + "," + stringAlt + "," + stringXAcel + "," + stringYAcel + "," + stringZAcel + "," 
+    + stringXGyro + "," + stringYGyro + "," + stringZGyro + ",");
+    Serial.println(dataString);
+    myFile.println(dataString);
     CurTime = millis();
-    if (CurTime >= TimeCheck)
+    if ((fabs((ZacelVal + 1) ) >= fabs(ZgUpper)) || (fabs((ZacelVal + 1) ) <= fabs(ZgLowwer)))
     {
-        launched = false;
-        Serial.print("LANDED!!!");
-        Serial.print('\t');
-    }
-    if ((fabs((ZacelVal + 1) ) <= fabs(ZgUpper)) && (fabs((ZacelVal + 1) ) >= fabs(ZgLowwer)))
-    {
-      Serial.print("Not Moving");
-      Serial.print('\t');
-    }
-    else
-    {
+
       ZgUpper = (ZacelVal + 1)  * 1.02;
       ZgLowwer = (ZacelVal + 1)  * 0.98;
       TimeCheck = CurTime + 2000;      
       Serial.print("Moving");
       Serial.print('\t');
     }
-    Serial.print(fabs(ZgLowwer));
-    Serial.print('\t');
-    Serial.print(fabs((ZacelVal + 1) ));
-    Serial.print('\t');
-    Serial.print(fabs(ZgUpper));
-    Serial.print('\t');
-    Serial.print((ZacelVal + 1)  <= ZgUpper);
-    Serial.print('\t');
-    Serial.print((ZacelVal + 1)  >= ZgLowwer);
-    Serial.print('\t');
-    Serial.print(((ZacelVal + 1)  <= ZgUpper) && ((ZacelVal + 1)  >= ZgLowwer));
-    Serial.print('\t');
-    Serial.println(" ");
+    else
+    {
+      Serial.print("Not Moving");
+      Serial.print('\t');
+    }
+    if (CurTime >= TimeCheck)
+    {
+        launched = false;
+        myFile.close();
+        Serial.print("LANDED!!!");
+        Serial.print('\t');
+    }
   }
+  Serial.println();
+  
 }
